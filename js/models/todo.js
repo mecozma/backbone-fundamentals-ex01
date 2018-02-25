9ca4e08b57afeec3b3bf87158772f19baa1d4ebe
mecozma <mecozma@gmail.com>
2018-02-25 23:14:22 +0000

    Added the Mdel Collection

--- js/models/todo.js
+++ js/models/todo.js
@@ -4,15 +4,15 @@ var app = app || {};
 // ----------
 // Our basic **Todo** model has `title` and `completed` attributes.
 app.Todo = Backbone.Model.extend({
-// Default attributes ensure that each todo created has `title` and `completed` keys.
-defaults: {
-title: '',
-completed: false
-},
-// Toggle the `completed` state of this todo item.
-toggle: function() {
-    this.save({
-    completed: !this.get('completed')
-    });
+    // Default attributes ensure that each todo created has `title` and `completed` keys.
+    defaults: {
+        title: '',
+        completed: false
+    },
+    // Toggle the `completed` state of this todo item.
+    toggle: function () {
+        this.save({
+            completed: !this.get('completed')
+        });
     }
-    });
+});
